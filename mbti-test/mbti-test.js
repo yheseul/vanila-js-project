89266826e7189b0e2c7c4a91a1b6faa45639a7ce
강예슬 <gang-yeseul@gang-yeseul-ui-MacBookAir.local>
2024-02-24 14:32:46 +0900

    feat: progress 요소를 통해 checkbox 체크 작업 완료 정도 나타내는 기능 구현

--- mbti-test/mbti-test.js
+++ mbti-test/mbti-test.js
@@ -1,9 +1,12 @@
+const mbtiForm=document.getElementById("mainForm");
+const progressBar=document.getElementById("progress-Bar");
 const checkboxes1=document.querySelectorAll(".checkbox1");
 const checkboxes2=document.querySelectorAll(".checkbox2");
 const checkboxes3=document.querySelectorAll(".checkbox3");
 const checkboxes4=document.querySelectorAll(".checkbox4");
 const submitButton=document.getElementById("button");
 const input=document.querySelectorAll("input");
+const questions=document.querySelectorAll(".question");
 
 function oneCheck(target, checkboxes) {
   checkboxes.forEach((checkbox) => {
@@ -33,6 +36,17 @@ checkboxes4.forEach((checkbox) => {
 });
 
 input.forEach((event)=>{
+    let count=0;
+    event.addEventListener("click",()=>{
+      const selctedCheckboxes=document.querySelectorAll('input:checked');
+      const selctedCheckboxesCount=selctedCheckboxes.length;
+    if(selctedCheckboxesCount>0){ 
+    progressBar.value=100*(selctedCheckboxesCount/questions.length);
+    }
+  })
+})
+
+input.forEach((event)=>{
   event.addEventListener("click",()=>{
     const selctedCheckboxes=document.querySelectorAll('input:checked');
     const selctedCheckboxesCount=selctedCheckboxes.length;
@@ -42,7 +56,7 @@ input.forEach((event)=>{
   })
 })
 
-  submitButton.addEventListener("click",()=>{
+submitButton.addEventListener("click",()=>{
     const selctedCheckboxes=document.querySelectorAll('input:checked');
     result ="";
     selctedCheckboxes.forEach((el)=>{
