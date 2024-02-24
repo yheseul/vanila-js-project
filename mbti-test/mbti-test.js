bfe955b6f0b48f8bd385d7f926241a7d262b239f
강예슬 <gang-yeseul@gang-yeseul-ui-MacBookAir.local>
2024-02-24 23:40:51 +0900

    refactor: 변수명 수정

--- mbti-test/mbti-test.js
+++ mbti-test/mbti-test.js
@@ -1,46 +1,46 @@
-const checkboxes1=document.querySelectorAll(".checkbox1");
-const checkboxes2=document.querySelectorAll(".checkbox2");
-const checkboxes3=document.querySelectorAll(".checkbox3");
-const checkboxes4=document.querySelectorAll(".checkbox4");
-const input=document.querySelectorAll("input");
-const progressBar=document.getElementById("progress-Bar");
-const questions=document.querySelectorAll(".question");
-const submitButton=document.getElementById("button");
+const checkboxes1 = document.querySelectorAll(".checkbox1");
+const checkboxes2 = document.querySelectorAll(".checkbox2");
+const checkboxes3 = document.querySelectorAll(".checkbox3");
+const checkboxes4 = document.querySelectorAll(".checkbox4");
+const input = document.querySelectorAll("input");
+const progressBar = document.getElementById("progress-Bar");
+const questions = document.querySelectorAll(".question");
+const submitButton = document.getElementById("button");
 
 submitButton.disabled = true;
 
-oneCheck=(target, checkboxes)=>{
+selectSingleCheckbox=(target, checkboxes)=>{
   checkboxes.forEach((checkbox) => checkbox.checked = false);
   target.checked = true;
 }
 
-unduplicatedCheck=(checkboxes)=>{
+attachExclusiveCheck=(checkboxes)=>{
   checkboxes.forEach((checkbox) => {
-    checkbox.addEventListener('click', () => oneCheck(checkbox, checkboxes));
+    checkbox.addEventListener('click', () => selectSingleCheckbox(checkbox, checkboxes));
   });
 }
 
-unduplicatedCheck(checkboxes1);
-unduplicatedCheck(checkboxes2);
-unduplicatedCheck(checkboxes3);
-unduplicatedCheck(checkboxes4);
+attachExclusiveCheck(checkboxes1);
+attachExclusiveCheck(checkboxes2);
+attachExclusiveCheck(checkboxes3);
+attachExclusiveCheck(checkboxes4);
 
-input.forEach((event)=>{
-  event.addEventListener("click",()=>{
-    const selctedCheckboxes=document.querySelectorAll('input:checked');
-    const selctedCheckboxesCount=selctedCheckboxes.length;
-    const questionsCount=questions.length;
-    if(selctedCheckboxesCount>0) progressBar.value=100*(selctedCheckboxesCount/questionsCount);
+input.forEach((event) => {
+  event.addEventListener("click", () => {
+    const selctedCheckboxes = document.querySelectorAll('input:checked');
+    const selctedCheckboxesCount = selctedCheckboxes.length;
+    const questionsCount = questions.length;
+    if(selctedCheckboxesCount > 0) progressBar.value = 100 * (selctedCheckboxesCount/questionsCount);
     if(selctedCheckboxesCount === 4){
-      submitButton.innerText="나의 MBTI 확인하기!";
+      submitButton.innerText = "나의 MBTI 확인하기!";
       submitButton.disabled = false;
     }
   })
 })
 
-submitButton.addEventListener("click",()=>{
-  result ="";
-  const selctedCheckboxes=document.querySelectorAll('input:checked');
-  selctedCheckboxes.forEach((el)=>result += el.value);
+submitButton.addEventListener("click", () => {
+  result = "";
+  const selctedCheckboxes = document.querySelectorAll('input:checked');
+  selctedCheckboxes.forEach((el) => result += el.value);
   alert(`당신의 MBTI 유형은 ${result}입니다.`);
 })
