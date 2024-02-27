c46bbbb55ca53eb5836839af9152fff47b4ce2b6
강예슬 <gang-yeseul@gang-yeseul-ui-MacBookAir.local>
2024-02-27 17:11:20 +0900

    feat: 문제 정답 확인 기능 구현

--- multiplicationTables/multiplicationTables.js
+++ multiplicationTables/multiplicationTables.js
@@ -2,24 +2,37 @@ const $question = document.getElementById('question');
 const $leftOperand = document.getElementById('leftOperand');
 const $rigthOperand = document.getElementById('rigthOperand');
 const $input = document.querySelector('input')
+const $submitButton = document.getElementById('submitButton');
 
 // 랜덤 숫자 생성 함수
 randomNumberGeneration = () => {
   return Math.floor(Math.random() * 10) + 1;
 }
 
+const leftOperand = randomNumberGeneration();
+const rigthOperand = randomNumberGeneration();
+
 // 구구단 문제 생성
-creatingMultiplicationProblem = (leftOperand ,rigthOperand) => {
+creatingMultiplicationProblem = () => {
   $leftOperand.innerHTML = leftOperand;
   $rigthOperand.innerHTML = rigthOperand;
 }
 
-// 랜덤 숫자 생성
-randomNumber = () => {
-  const leftOperand = randomNumberGeneration();
-  const rigthOperand = randomNumberGeneration();
+creatingMultiplicationProblem();
 
-  creatingMultiplicationProblem(leftOperand, rigthOperand);
+// 제출 버튼 클릭 이벤트 핸들러
+handleSubmitButtonClick = () => {
+  const answerQuestion = leftOperand * rigthOperand;
+  const answerEnteredUser = $input.value;
+
+  checkAnswerQuestion(answerQuestion, answerEnteredUser);
 }
 
-randomNumber();
+// 문제 정답 결과 메세지 출력
+checkAnswerQuestion = (answerQuestion, answerEnteredUser) => {
+  if(answerQuestion != answerEnteredUser) alert('틀렸습니다.');
+  if(answerQuestion == answerEnteredUser) alert('정답입니다.');
+}
+
+// 제출 버튼에 이벤트 리스너 등록
+submitButton.addEventListener("click", handleSubmitButtonClick);
