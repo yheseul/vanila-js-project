da266e9664061fe4c8f31f3968812321747ed6b0
강예슬 <gang-yeseul@gang-yeseul-ui-MacBookAir.local>
2024-02-27 18:25:51 +0900

    feat: 정답 결과 카운트 기능 구현

--- multiplicationTables/multiplicationTables.js
+++ multiplicationTables/multiplicationTables.js
@@ -3,6 +3,8 @@ const $leftOperand = document.getElementById('leftOperand');
 const $rigthOperand = document.getElementById('rigthOperand');
 const $input = document.querySelector('input')
 const $submitButton = document.getElementById('submitButton');
+const $correctQuestionCount = document.getElementById('correctQuestionCount');
+const $wrongQuestionCount = document.getElementById('wrongQuestionCount');
 
 // 랜덤 숫자 생성 함수
 randomNumberGeneration = () => {
@@ -28,10 +30,27 @@ handleSubmitButtonClick = () => {
   checkAnswerQuestion(answerQuestion, answerEnteredUser);
 }
 
+// 틀린 문제 개수
+numberQuestionsIncorrect = () => {
+  const currentWrongQuestionCount=parseInt($wrongQuestionCount.innerText, 10);
+  $wrongQuestionCount.innerHTML = currentWrongQuestionCount + 1;
+}
+// 맞춘 문제 개수
+numberQuestionsMatched = () => {
+  const currentCorrectQuestionCount=parseInt($correctQuestionCount.innerText, 10);
+  correctQuestionCount.innerHTML = currentCorrectQuestionCount + 1;
+}
+
 // 문제 정답 결과 메세지 출력
 checkAnswerQuestion = (answerQuestion, answerEnteredUser) => {
-  if(answerQuestion != answerEnteredUser) alert('틀렸습니다.');
-  if(answerQuestion == answerEnteredUser) alert('정답입니다.');
+  if(answerQuestion != answerEnteredUser) {
+    alert('틀렸습니다.');
+    numberQuestionsIncorrect();
+  }
+  if(answerQuestion == answerEnteredUser) {
+    alert('정답입니다.');
+    numberQuestionsMatched();
+  }
 }
 
 // 문제 정답 제출 이벤트 리스너 복수 등록
