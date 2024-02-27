26ff9113aa6ba52c25ab7bbadfd39db5c3f8d3c5
강예슬 <gang-yeseul@gang-yeseul-ui-MacBookAir.local>
2024-02-27 16:53:52 +0900

    refactor: 구구단 문제 랜덤 생성 기능 리팩토링

--- multiplicationTables/multiplicationTables.js
+++ multiplicationTables/multiplicationTables.js
@@ -1,16 +1,25 @@
 const $question = document.getElementById('question');
 const $leftOperand = document.getElementById('leftOperand');
 const $rigthOperand = document.getElementById('rigthOperand');
+const $input = document.querySelector('input')
 
-// 랜덤 숫자 생성
+// 랜덤 숫자 생성 함수
 randomNumberGeneration = () => {
   return Math.floor(Math.random() * 10) + 1;
 }
 
 // 구구단 문제 생성
-creatingMultiplicationProblem = () => {
-  $leftOperand.innerHTML = `${randomNumberGeneration()}`;
-  $rigthOperand.innerHTML = `${randomNumberGeneration()}`;
+creatingMultiplicationProblem = (leftOperand ,rigthOperand) => {
+  $leftOperand.innerHTML = leftOperand;
+  $rigthOperand.innerHTML = rigthOperand;
 }
 
-creatingMultiplicationProblem();
+// 랜덤 숫자 생성
+randomNumber = () => {
+  const leftOperand = randomNumberGeneration();
+  const rigthOperand = randomNumberGeneration();
+
+  creatingMultiplicationProblem(leftOperand, rigthOperand);
+}
+
+randomNumber();
