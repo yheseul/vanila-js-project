58f6f887fc9e6ad826b9d1b250b300f24e5d4070
강예슬 <gang-yeseul@gang-yeseul-ui-MacBookAir.local>
2024-02-29 19:13:14 +0900

    refactor: 랜덤 숫자 생성 기능 리팩토링

--- multiplicationTables/multiplicationTables.js
+++ multiplicationTables/multiplicationTables.js
@@ -5,14 +5,23 @@ const $input = document.querySelector('input')
 const $submitButton = document.getElementById('submitButton');
 const $correctQuestionCount = document.getElementById('correctQuestionCount');
 const $wrongQuestionCount = document.getElementById('wrongQuestionCount');
+const $solvingOtherProblemsButton = document.getElementById('solvingOtherProblemsButton');
 
 // 랜덤 숫자 생성 함수
 randomNumberGeneration = () => {
   return Math.floor(Math.random() * 10) + 1;
 }
 
-const leftOperand = randomNumberGeneration();
-const rigthOperand = randomNumberGeneration();
+// 랜덤 숫자
+randomNumber = () => {
+  const leftOperand = randomNumberGeneration();
+  const rigthOperand = randomNumberGeneration();
+
+  return [leftOperand, rigthOperand];
+}
+
+// 생성된 랜덤 숫자를 담은 배열 디스트럭처링 할당
+const [leftOperand, rigthOperand] = randomNumber();
 
 // 구구단 문제 생성
 creatingMultiplicationProblem = () => {
@@ -20,29 +29,18 @@ creatingMultiplicationProblem = () => {
   $rigthOperand.innerHTML = rigthOperand;
 }
 
-creatingMultiplicationProblem();
+creatingMultiplicationProblem()
 
 // 문제 정답 제출 이벤트 핸들러
 handleSubmitButtonClick = () => {
   const answerQuestion = leftOperand * rigthOperand;
   const answerEnteredUser = $input.value;
 
-  checkAnswerQuestion(answerQuestion, answerEnteredUser);
-}
-
-// 틀린 문제 개수
-numberQuestionsIncorrect = () => {
-  const currentWrongQuestionCount=parseInt($wrongQuestionCount.innerText, 10);
-  $wrongQuestionCount.innerHTML = currentWrongQuestionCount + 1;
-}
-// 맞춘 문제 개수
-numberQuestionsMatched = () => {
-  const currentCorrectQuestionCount=parseInt($correctQuestionCount.innerText, 10);
-  correctQuestionCount.innerHTML = currentCorrectQuestionCount + 1;
+  checkAnswerQuestionMessage(answerQuestion, answerEnteredUser);
 }
 
 // 문제 정답 결과 메세지 출력
-checkAnswerQuestion = (answerQuestion, answerEnteredUser) => {
+checkAnswerQuestionMessage = (answerQuestion, answerEnteredUser) => {
   if(answerQuestion != answerEnteredUser) {
     alert('틀렸습니다.');
     numberQuestionsIncorrect();
@@ -51,6 +49,18 @@ checkAnswerQuestion = (answerQuestion, answerEnteredUser) => {
     alert('정답입니다.');
     numberQuestionsMatched();
   }
+}
+
+// 틀린 문제 개수
+numberQuestionsIncorrect = () => {
+  const currentWrongQuestionCount=parseInt($wrongQuestionCount.innerText, 10);
+  $wrongQuestionCount.innerHTML = currentWrongQuestionCount + 1;
+}
+
+// 맞춘 문제 개수
+numberQuestionsMatched = () => {
+  const currentCorrectQuestionCount=parseInt($correctQuestionCount.innerText, 10);
+  $correctQuestionCount.innerHTML = currentCorrectQuestionCount + 1;
 }
 
 // 제출 버튼 클릭 이벤트 리스너 등록
