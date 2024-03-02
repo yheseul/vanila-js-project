e328c21b42f593b1921eee3f03bb1955d1f8d1b0
강예슬 <gang-yeseul@gang-yeseul-ui-MacBookAir.local>
2024-03-02 19:17:58 +0900

    comment: 주석 제거

--- multiplicationTables/multiplicationTables.js
+++ multiplicationTables/multiplicationTables.js
@@ -7,12 +7,10 @@ const $correctQuestionCount = document.getElementById('correctQuestionCount');
 const $wrongQuestionCount = document.getElementById('wrongQuestionCount');
 const $solvingOtherProblemsButton = document.getElementById('solvingOtherProblemsButton');
 
-// 랜덤 숫자 생성 함수
 randomNumberGeneration = () => {
   return Math.floor(Math.random() * 10) + 1;
 }
 
-// 구구단 문제 생성
 randomNumber = () => {
   const leftOperand = randomNumberGeneration();
   const rigthOperand = randomNumberGeneration();
@@ -20,7 +18,6 @@ randomNumber = () => {
   creatingMultiplicationProblem(leftOperand, rigthOperand);
 }
 
-// 구구단 문제 화면에 출력
 creatingMultiplicationProblem = (leftOperand, rigthOperand) => {
   $leftOperand.innerHTML = leftOperand;
   $rigthOperand.innerHTML = rigthOperand;
@@ -28,7 +25,6 @@ creatingMultiplicationProblem = (leftOperand, rigthOperand) => {
 
 randomNumber();
 
-// 문제 정답 제출 이벤트 핸들러
 handleSubmitButtonClick = (event) => {
   event.preventDefault();
 
@@ -41,7 +37,6 @@ handleSubmitButtonClick = (event) => {
   $input.value = '';
 }
 
-// 문제 답 확인
 checkAnswerQuestion = (answerQuestion, answerEnteredUser) => {
   if(answerQuestion != answerEnteredUser) {
     wrongAnswerMessage();
@@ -54,41 +49,33 @@ checkAnswerQuestion = (answerQuestion, answerEnteredUser) => {
   }
 }
 
-// 오답 메세지
 wrongAnswerMessage = () => {
   alert('틀렸습니다.');
 }
 
-// 정답 메세지
 answerMessage = () => {
   alert('정답입니다.');
 }
 
-// 틀린 문제 개수
 numberQuestionsIncorrect = () => {
   const currentWrongQuestionCount=parseInt($wrongQuestionCount.innerText, 10);
   $wrongQuestionCount.innerHTML = currentWrongQuestionCount + 1;
 }
 
-// 맞춘 문제 개수
 numberQuestionsMatched = () => {
   const currentCorrectQuestionCount=parseInt($correctQuestionCount.innerText, 10);
   $correctQuestionCount.innerHTML = currentCorrectQuestionCount + 1;
 }
 
-// 다른 문제 풀기 버튼 클릭 시 새로운 문제로 업데이트
 askDifferentQuestion = (event) => {
   event.preventDefault();
   randomNumber();
 }
 
-// 다른 문제 풀기 버튼 클릭 이벤트 리스너 등록
 $solvingOtherProblemsButton.addEventListener("click", askDifferentQuestion);
 
-// 제출 버튼 클릭 이벤트 리스너 등록
 $submitButton.addEventListener("click", handleSubmitButtonClick);
 
-// 입력창 엔터키 이벤트 리스너 등록
 $input.addEventListener("keypress", (event) => {
   if(event.key === 'Enter') handleSubmitButtonClick(event);
 });
