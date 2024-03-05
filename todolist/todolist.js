c73e7c26e59027d64d1ca74f589275a6d183a986
강예슬 <gang-yeseul@gang-yeseul-ui-MacBookAir.local>
2024-03-05 20:06:30 +0900

    refactor: 할 일 추가 기능 리팩토링

--- todolist/todolist.js
+++ todolist/todolist.js
@@ -6,19 +6,24 @@ const $remainingDoCount = document.getElementById('remainingDoCount');
 addToDoList = (event) => {
   event.preventDefault();
 
+  createToDoList();
+  increasedRemainingDoCount();
+
+  $toDoInput.value = "";
+}
+
+createToDoList = () => {
   const newToDo = $toDoInput.value;
   const liElement = document.createElement('li');
+
+  liElement.style.listStyle = "none";
 
   const creatingInputValueTextNode = document.createTextNode(newToDo);
   liElement.appendChild(creatingInputValueTextNode);
   $toDoList.appendChild(liElement);
-  
-  increasedRemainingDoCount();
-
-  $toDoInput.value = " ";
 }
 
-increasedRemainingDoCount = () =>{
+increasedRemainingDoCount = () => {
   const CurrentRemainingDoCount = parseInt($remainingDoCount.innerHTML, 10);
   $remainingDoCount.innerHTML = CurrentRemainingDoCount + 1;
 }
