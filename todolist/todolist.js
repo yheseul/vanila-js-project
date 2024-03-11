ba47d306468a2483a84771cd4b2af9b1f9f87c46
강예슬 <gang-yeseul@gang-yeseul-ui-MacBookAir.local>
2024-03-11 19:45:42 +0900

    feat: 삭제하고 싶은 할 일의 체크박스 선택 후 ‘-‘ 버튼 클릭 시 리스트에서 삭제되는 기능과 삭제된 개수만큼 남은 할 일 개수 감소하는 기능 구현

--- todolist/todolist.js
+++ todolist/todolist.js
@@ -27,13 +27,37 @@ displayNewToDo = (liElement, toDoCheckbox, toDoText) => {
   if(toDoText) {
     liElement.append(toDoCheckbox, toDoText);
     $toDoList.appendChild(liElement);
-    increasecountRemainingToDo();
+    increaseCountRemainingToDo();
   }
 }
 
-increasecountRemainingToDo = () => {
+increaseCountRemainingToDo = () => {
   const currentRemainingToDoCount = parseInt($remainingToDoCount.innerHTML, 10);
   $remainingToDoCount.innerHTML = currentRemainingToDoCount + 1;
 }
 
+decreaseCountRemainingToDo = (numberDeletedToDo) => {
+  const currentRemainingToDoCount = parseInt($remainingToDoCount.innerHTML, 10);
+  $remainingToDoCount.innerHTML = currentRemainingToDoCount - numberDeletedToDo;
+}
+
+deleteToDoList = () => {
+  const toDolist = $toDoList.querySelectorAll('li');
+  const checkedboxes = $toDoList.querySelectorAll('input');
+  let anyCheckboxChecked = false;
+
+  for (let i in toDolist) {
+    if (checkedboxes[i].checked) {
+      anyCheckboxChecked = true;
+      toDolist[i].remove();
+      decreaseCountRemainingToDo(1);
+    }
+  }
+
+  if(!anyCheckboxChecked){
+    alert("Please select the list you want to delete.");
+  }
+}
+
 $addButton.addEventListener("click", addNewToDoList);
+$deleteButton.addEventListener("click", deleteToDoList);
