524e0de555fb27d0d0953202b5babbdac0278143
강예슬 <gang-yeseul@gang-yeseul-ui-MacBookAir.local>
2024-03-16 02:26:38 +0900

    feat: 연산 과정 보여주는 기능 추가 / refactor: 코드 전면 리팩토링

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,12 +2,40 @@ const $ = (id) => document.getElementById(id);
 const $number = document.querySelectorAll('.number');
 const $operator = document.querySelectorAll('.operator');
 
+let leftOperand = '';
+let rightOperand = '';
+let operator = '';
+
 numberClickEvent = (event) => {
-  $('result').innerText += event.target.innerText;
+  const clickedNumber = event.target.innerText;
+  if(operator === '') createLeftOperand(clickedNumber);
+  if(operator !== '') createRightOperand(clickedNumber);
+}
+
+createLeftOperand = (clickedNumber) => {
+  $('result').innerText += clickedNumber;
+  let currentNumber = $('result').innerText;
+  leftOperand = currentNumber;
+  calculationProcess(clickedNumber);
+}
+
+createRightOperand = (clickedNumber) => {
+  $('result').innerText += clickedNumber;
+  let currentNumber = $('result').innerText;
+  rightOperand = currentNumber;
+  calculationProcess(clickedNumber);
 }
 
 operatorClickEvent = (event) => {
-  $('result').innerText = event.target.innerText;
+  const clickedOperator = event.target.innerText;
+  operator = clickedOperator;
+  if(clickedOperator === "=") calculationResult();
+  else calculationProcess(clickedOperator);
+  $('result').innerText = '';
+}
+
+calculationProcess = (clickedButton) => {
+  $('calculationProcessWindow').innerText += clickedButton;
 }
 
 $number.forEach((number) => {
@@ -16,7 +44,7 @@ $number.forEach((number) => {
   })
 })
 
-$operation.forEach((operator) => {
+$operator.forEach((operator) => {
   operator.addEventListener("click", (event) => {
     operatorClickEvent(event);
   })
